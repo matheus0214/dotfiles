b2ef00e11ed2fcce6ea2932899d3dce8d7e94da7
Matheus G. Dias <matheusgiga123@gmail.com>
2025-04-16 16:03:58 -0300

    create a install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/sh
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+
+rm lazygit
+rm lazygit.tar.gz
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+echo '\nexport PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
+
+rm nvim-linux-x86_64.tar.gz
+
+sudo apt-get install ripgrep -y
+
+sudo apt install stow -y
+
+stow .
+
