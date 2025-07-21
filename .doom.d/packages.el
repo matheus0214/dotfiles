c17e0103f067a8b4df6a610ca9a75530ce52c9ec
Matheus G. Dias <matheusgiga123@gmail.com>
2025-07-20 21:06:32 -0300

    add emacs odin integration

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -47,3 +47,6 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
+
+(package! odin-mode
+  :recipe (:host github :repo "mattt-b/odin-mode"))
