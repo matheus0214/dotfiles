af4328acb165721702df9f9b1acdc8c8dff4111b
Matheus G. Dias <matheusgiga123@gmail.com>
2025-07-31 19:47:07 -0300

    emacs configs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -32,16 +32,15 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'wombat)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 'relative)
+;; (setq display-line-numbers-type 'relative)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
@@ -79,9 +78,15 @@
 
 (setq confirm-kill-emacs nil) ;; disable ask if want to quit
 (xterm-mouse-mode 1)
+(remove-hook 'doom-first-buffer-hook #'global-hl-line-mode)
+(global-hl-line-mode -1)
 
 ;; Key maps
 
 (map! :leader
       :desc "Search file (consult-find)"
       "f s" #'consult-find)
+
+(map! :leader
+      :desc "Grep files"
+      "/" #'consult-ripgrep)
